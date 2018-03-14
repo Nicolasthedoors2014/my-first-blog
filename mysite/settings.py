070613e1ab61b687fb0068d9659bf4bde81916a7
Nicolas Aguilar <nicolasthedoors2014@gmail.com>
2018-03-13 22:52:42 -0300

    edit ALLOWED_HOSTS

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'l&l%2rh8$d^*8x(m_war@%etw)6yvmav(m=nu1ic5(#=c137=q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
